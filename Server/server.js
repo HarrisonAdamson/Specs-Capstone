909a8dcd3bebfa5e614de22eea10bc40b2ff62d4
HarrisonAdamson <harryadamson2001@gmail.com>
2023-07-16 20:39:48 -0600

    Added comment on how to start server on local

--- Server/server.js
+++ Server/server.js
@@ -1,18 +1,25 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const {SERVER_PORT} = process.env
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { SERVER_PORT } = process.env;
 app.use(express.json());
 
-const {seed, getRating, createRating, deleteRating} = require('./controller.js')
+const {
+  seed,
+  getRating,
+  createRating,
+  deleteRating,
+} = require("./controller.js");
 
 app.use(cors());
 
-app.post('/seed', seed)
+app.post("/seed", seed);
 
-app.post('/ratings', createRating)
-app.get('/ratings', getRating)
-app.delete('/ratings/:id', deleteRating)
+app.post("/ratings", createRating);
+app.get("/ratings", getRating);
+app.delete("/ratings/:id", deleteRating);
 
-app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`))
+app.listen(SERVER_PORT, () => console.log(`up on ${SERVER_PORT}`));
+
+//start server with node server.js/server
